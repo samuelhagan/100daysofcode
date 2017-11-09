227c838cc74e990fbb723751a83c13ac94880d00
samuelhagan <hagansamuel6@gmail.com>
2017-11-09 19:44:38 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     // rest of the JavaScript code
+//     $('#js-sign-btn').on('click', function(e) {
+//         $(e.currentTarget).closest('ul').hide();
+//         $('form#js-signin').toggleClass('hidden-element').fadeIn('fast');
+//     });
+// });
